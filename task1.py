439744b55a6a46f0602c4e5484e1c9e128b9c132
YashD2002 <136880720+YashD2002@users.noreply.github.com>
2025-04-06 20:32:29 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,6 @@
+x = int(input("Enter an integer: "))
+
+if x % 2 == 0:
+    print(f"The {x} is an even number.")
+else:
+    print(f"The {x} is an odd number.")
---
+++ task2.py
@@ -0,0 +1,6 @@
+p = 0
+
+for x in range(1, 51):
+    p += x
+
+print("The sum of integers from 1 to 50 is:", p)
